f6eed2f4c54ccff0556b8de886b4c28768f30f17
mnmhbbb <mnmhb64@gmail.com>
2025-10-29 16:38:22 +0900

    Chore: 프로젝트 초기 세팅

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginUnusedImports from "eslint-plugin-unused-imports";
+import esPluginImport from "eslint-plugin-import";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      import: esPluginImport,
+      "unused-imports": eslintPluginUnusedImports,
+    },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      "import/order": [
+        "warn",
+        {
+          groups: [["builtin", "external"], "internal", ["parent", "sibling", "index"]],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "import/no-duplicates": "error", // 중복 import 방지
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+]);
