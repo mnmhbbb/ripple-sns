1cb80b3f471bc0e44442a590c456c80acc0ea6e6
mnmhbbb <mnmhb64@gmail.com>
2025-10-31 18:18:43 +0900

    Chore: Tanstack Query, devtools 설치 및 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 import "./index.css";
-
 import App from "./App.tsx";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
