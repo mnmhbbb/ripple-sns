c799d8e3f7b3f8b388988cecbb28740a8d2544c2
mnmhbbb <mnmhb64@gmail.com>
2025-11-05 16:16:04 +0900

    Feat: 로그인/비로그인 전용 페이지에 접근 권한 처리 추가(라우트 가드)

--- src/root-route.tsx
+++ src/root-route.tsx
@@ -1,6 +1,8 @@
 import { Navigate, Route, Routes } from "react-router";
 
 import GlobalLayout from "@/components/layout/global-layout";
+import GuestOnlyLayout from "@/components/layout/guest-only-layout";
+import MemberOnlyLayout from "@/components/layout/member-only-layout";
 import ForgetPasswordPage from "@/pages/forget-password-page";
 import IndexPage from "@/pages/index-page";
 import PostDetailPage from "@/pages/post-detail-page";
@@ -12,15 +14,19 @@ import SignUpPage from "@/pages/sign-up-page";
 export default function RootRoute() {
   return (
     <Routes>
-      <Route path="/" element={<GlobalLayout />}>
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/forget-password" element={<ForgetPasswordPage />} />
+      <Route element={<GlobalLayout />}>
+        <Route element={<GuestOnlyLayout />}>
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        </Route>
 
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/post/:postId" element={<PostDetailPage />} />
-        <Route path="/profile/:userId" element={<ProfileDetailPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route element={<MemberOnlyLayout />}>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/post/:postId" element={<PostDetailPage />} />
+          <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+        </Route>
 
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
