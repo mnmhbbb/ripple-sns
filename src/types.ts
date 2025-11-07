9a61d2f757e3d84e59f2cd9fa0f1fa70666f8a3f
mnmhbbb <mnmhb64@gmail.com>
2025-11-08 03:23:30 +0900

    feat: 포스트와 프로필 테이블 간의 외래키 추가
    - PostEntity에 author 필드를 추가하여 Post와 Profile 간의 관계를 명시적으로 정의
    - Database 타입에 post와 profile 간의 관계를 설정하여 데이터 구조 개선

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,10 @@ import { type Database } from "@/lib/database.types";
 export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
 export type ProfileEntity = Database["public"]["Tables"]["profile"]["Row"];
 
+export type Post = PostEntity & {
+  author: ProfileEntity;
+};
+
 export type UseMutationCallbacks = {
   onError?: (error: Error) => void;
   onSuccess?: () => void;
