1b0f8305f9f9517d55520daa3ab48b35aad85053
mnmhbbb <mnmhb64@gmail.com>
2025-11-22 20:15:13 +0900

    Docs: useIsSessionLoaded에 대한 설명 주석 추가

--- src/store/session.ts
+++ src/store/session.ts
@@ -32,6 +32,10 @@ export const useSession = () => {
   return session;
 };
 
+// onAuthStateChange는 최초 마운트 이후에 실행되기 때문에
+// 최초 마운트 시점에는 세션이 없어서 페이지 권한이 없다고 튕겨지거나, 잠시 어색하게 버튼이 생겼다가 없어지는 현상이 발생할 수 있음
+// 따라서 isLoaded 상태를 사용하여 최초 마운트 시점에는 로딩 중임을 표시하기 위함
+// session-provider에서 사용함
 export const useIsSessionLoaded = () => {
   const isLoaded = useSessionStore((state) => state.isLoaded);
   return isLoaded;
