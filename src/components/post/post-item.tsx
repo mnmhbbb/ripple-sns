dd85ed6efa2d19ee3b6b082ed150cb4a7a58e804
mnmhbbb <mnmhb64@gmail.com>
2025-11-28 18:59:22 +0900

    Feat: 포스트 수정 및 삭제 버튼에 조건부 렌더링 추가
    - useSession 훅을 사용하여 현재 사용자 세션 정보를 가져오고, 포스트 작성자와 비교하여 수정 및 삭제 버튼의 표시 여부를 결정하도록 변경
    - 작성자가 본인인 경우에만 EditPostButton과 DeletePostButton이 렌더링되도록 수정

--- src/components/post/post-item.tsx
+++ src/components/post/post-item.tsx
@@ -9,9 +9,14 @@ import {
   CarouselItem,
 } from "@/components/ui/carousel";
 import { formatTimeAgo } from "@/lib/time";
+import { useSession } from "@/store/session";
 import type { Post } from "@/types";
 
 export default function PostItem(post: Post) {
+  const session = useSession();
+  const userId = session?.user?.id;
+  const isMine = userId === post.author_id;
+
   return (
     <div className="flex flex-col gap-4 border-b pb-8">
       {/* 1. 유저 정보, 수정/삭제 버튼 */}
@@ -35,8 +40,12 @@ export default function PostItem(post: Post) {
 
         {/* 1-2. 수정/삭제 버튼 */}
         <div className="text-muted-foreground flex text-sm">
-          <EditPostButton {...post} />
-          <DeletePostButton id={post.id} />
+          {isMine && (
+            <>
+              <EditPostButton {...post} />
+              <DeletePostButton id={post.id} />
+            </>
+          )}
         </div>
       </div>
 
