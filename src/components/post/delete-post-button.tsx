601a1a69652dbd8a76fea6dd80798200124e0ad3
mnmhbbb <mnmhb64@gmail.com>
2025-12-01 17:43:28 +0900

    Feat: DeletePostButton 컴포넌트에 삭제 후 리다이렉트 기능 추가
    - useNavigate 훅을 사용하여 포스트 삭제 후 해당 포스트의 상세 페이지에서 메인 페이지로 리다이렉트하도록 수정

--- src/components/post/delete-post-button.tsx
+++ src/components/post/delete-post-button.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router";
 import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
@@ -5,8 +6,17 @@ import useDeletePost from "@/hooks/mutations/post/use-delete-post";
 import { useOpenAlertModal } from "@/store/alert-modal";
 
 export default function DeletePostButton({ id }: { id: number }) {
+  const navigate = useNavigate();
+
   const openAlertModal = useOpenAlertModal();
   const { mutate: deletePost, isPending: isDeletePostPending } = useDeletePost({
+    onSuccess: () => {
+      // 포스트 상세 페이지에서 삭제 시 메인 페이지로 이동
+      const pathname = window.location.pathname;
+      if (pathname.startsWith(`/post/${id}`)) {
+        navigate("/", { replace: true });
+      }
+    },
     onError: () => {
       toast.error("포스트 삭제에 실패했습니다.", {
         position: "top-center",
