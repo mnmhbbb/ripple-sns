4e4fb4eea1417d0af57ef97132b4334559dcfebe
mnmhbbb <mnmhb64@gmail.com>
2025-12-01 23:47:49 +0900

    Feat: 댓글 목록 데이터 fetching 기능 추가
    - fetchComments 함수를 추가하여 특정 포스트의 댓글을 Supabase에서 가져오는 기능 구현
    - CommentList 컴포넌트에서 댓글 데이터를 가져오기 위해 useCommentsData 훅을 사용하도록 수정
    - CommentItem 컴포넌트에 props를 통해 댓글 정보를 전달하여 UI를 동적으로 업데이트
    - PostDetailPage에서 CommentList에 postId prop을 전달하여 댓글 목록을 올바르게 렌더링

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -1,5 +1,16 @@
 import supabase from "@/lib/supabase";
 
+export async function fetchComments(postId: number) {
+  const { data, error } = await supabase
+    .from("comment")
+    .select("*, author: profile!author_id (*) ") // author 필드에 comment.author_id = profile.id 조건으로 조인한 profile 레코드를 중첩 객체로 가져옴(댓글에 프로필 정보를 가져와야 하기 때문)
+    .eq("post_id", postId)
+    .order("created_at", { ascending: false });
+
+  if (error) throw error;
+  return data;
+}
+
 export async function createComment({
   content,
   postId,
--- src/components/comment/comment-list.tsx
+++ src/components/comment/comment-list.tsx
@@ -1,11 +1,24 @@
-import CommentItem from "@/components/comment/comment-item";
+import Fallback from "../fallback";
+import Loader from "../loader";
 
-export default function CommentList() {
+import CommentItem from "@/components/comment/comment-item";
+import useCommentsData from "@/hooks/queries/use-comments-data";
+
+export default function CommentList({ postId }: { postId: number }) {
+  const {
+    data: comments,
+    error: fetchCommentsError,
+    isPending: isFetchingCommentsPending,
+  } = useCommentsData(postId);
+
+  if (isFetchingCommentsPending) return <Loader />;
+  if (fetchCommentsError) return <Fallback />;
+
   return (
     <div className="flex flex-col gap-5">
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
+      {comments?.map((comment) => (
+        <CommentItem key={comment.id} {...comment} />
+      ))}
     </div>
   );
 }
