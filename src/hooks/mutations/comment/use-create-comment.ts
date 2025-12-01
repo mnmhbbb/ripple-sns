3eaeb760dad57a9221e38a330a742c70da76cdeb
mnmhbbb <mnmhb64@gmail.com>
2025-12-02 00:57:20 +0900

    Feat: 댓글 생성 시 캐시 업데이트 기능 추가
    - useCreateComment 훅에서 댓글 생성 성공 시 프로필 정보를 포함하여 댓글 목록에 추가하도록 수정
    - useQueryClient를 사용하여 댓글 목록의 캐시를 업데이트하는 로직 추가
    - useProfileData 훅을 통해 현재 사용자의 프로필 정보를 가져오는 기능 통합

--- src/hooks/mutations/comment/use-create-comment.ts
+++ src/hooks/mutations/comment/use-create-comment.ts
@@ -1,13 +1,28 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { createComment } from "@/api/comment";
-import type { UseMutationCallbacks } from "@/types";
+import useProfileData from "@/hooks/queries/use-profile-data";
+import { QUERY_KEYS } from "@/lib/constants";
+import { useSession } from "@/store/session";
+import type { Comment, UseMutationCallbacks } from "@/types";
 
 export default function useCreateComment(callbacks?: UseMutationCallbacks) {
+  const queryClient = useQueryClient();
+  const session = useSession();
+  const { data: profile } = useProfileData(session?.user.id);
+
   return useMutation({
     mutationFn: createComment,
-    onSuccess: () => {
+    onSuccess: (newComment) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+      queryClient.setQueryData<Comment[]>(
+        QUERY_KEYS.comment.post(newComment.post_id),
+        (prevComments) => {
+          if (!prevComments) throw new Error("댓글이 존재하지 않습니다.");
+          if (!profile) throw new Error("프로필이 존재하지 않습니다.");
+          return [{ ...newComment, author: profile }, ...prevComments];
+        },
+      );
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
